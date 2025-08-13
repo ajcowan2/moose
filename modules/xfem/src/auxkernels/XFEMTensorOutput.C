643c895e91804463aa7fe3f111fab2e796d2bbf3
ajcowan2 <ajcowan2@ncsu.edu>
2025-08-13 18:43:47 -0400

    Cherry-picked correction factor changes from old repo

---
+++ modules/xfem/src/auxkernels/XFEMTensorOutput.C
@@ -0,0 +1,136 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "XFEMTensorOutput.h"
+#include "petscblaslapack.h"
+
+#include "XFEM.h"
+
+registerMooseObject("XFEMApp", XFEMTensorOutput);
+
+InputParameters
+XFEMTensorOutput::validParams()
+{
+  InputParameters params = AuxKernel::validParams();
+  params.addClassDescription(
+      "Computes the volume fraction of the physical material in each partial element.");
+
+  params.addParam<unsigned int>(
+      "i", 0, "First index of the stress tensor component (0, 1, 2)");
+
+  params.addParam<unsigned int>(
+      "j", 0, "Second index of the stress tensor component (0, 1, 2)");
+
+  return params;
+}
+
+XFEMTensorOutput::XFEMTensorOutput(const InputParameters & parameters)
+  : AuxKernel(parameters), 
+    _stress(getMaterialPropertyByName<RankTwoTensor>("stress")),
+    _i(getParam<unsigned int>("i")),
+    _j(getParam<unsigned int>("j"))
+{
+  if (isNodal())
+    mooseError("XFEMTensorOutput must be run on an element variable");
+  FEProblemBase * fe_problem = dynamic_cast<FEProblemBase *>(&_subproblem);
+  if (fe_problem == nullptr)
+    mooseError("Problem casting _subproblem to FEProblemBase in XFEMTensorOutput");
+  _xfem = MooseSharedNamespace::dynamic_pointer_cast<XFEM>(fe_problem->getXFEM());
+  if (_xfem == nullptr)
+    mooseError("Problem casting to XFEM in XFEMTensorOutput");
+  if (_i > 2 || _j > 2)
+    mooseError("Invalid tensor component: i and j must be 0, 1, or 2 for RankTwoTensor.");
+}
+
+Real
+XFEMTensorOutput::computeValue()
+{
+  return _xfem->getPhysicalVolumeFraction(_current_elem);
+}
+
+void
+XFEMTensorOutput::compute()
+{
+  precalculateValue();
+  if (_xfem->isElemCut(_current_elem))
+  {
+    Point cp = _xfem->getPhyiscalCenterPoint(_current_elem);
+
+    unsigned int N = _qrule->n_points();
+
+    std::vector<Real> known_values;
+    known_values.resize(N);
+    for (unsigned int i = 0; i < N; ++i)
+    {
+      known_values[i] = _stress[i](_i, _j);
+    }
+
+    // Build A matrix
+    std::vector<std::vector<Real>> A(N, std::vector<double>(4, 0.0));
+    for (unsigned int i = 0; i < N; ++i)
+    {
+      Real x = _q_point[i](0);
+      Real y = _q_point[i](1);
+      A[i][0] = 1.0;
+      A[i][1] = x;
+      A[i][2] = y;
+      A[i][3] = x * y;
+    }
+
+    // Solve least squares: (A^T * A) * coeffs = A^T * b
+    std::vector<std::vector<double>> AtA(4, std::vector<double>(4, 0.0));
+    std::vector<double> Atb(4, 0.0);
+
+    for (unsigned int i = 0; i < N; ++i)
+    {
+      for (int m = 0; m < 4; ++m)
+      {
+        Atb[m] += A[i][m] * known_values[i];
+        for (int n = 0; n < 4; ++n)
+        {
+          AtA[m][n] += A[i][m] * A[i][n];
+        }
+      }
+    }
+
+    // Solve system AtA * coeffs = Atb using LAPACK
+    std::vector<double> flatA(16, 0.0); // 4x4 matrix, column-major for LAPACK
+    for (int col = 0; col < 4; ++col)
+    {
+      for (int row = 0; row < 4; ++row)
+      {
+        flatA[col * 4 + row] = AtA[row][col]; // Column-major
+      }
+    }
+
+    std::vector<double> b = Atb; // copy
+    std::vector<int> ipiv(4, 0);
+
+    int n = 4;
+    int nrhs = 1;
+    int lda = 4;
+    int ldb = 4;
+    int info = 0;
+
+    LAPACKgesv_(&n, &nrhs, flatA.data(), &lda, ipiv.data(), b.data(), &ldb, &info);
+
+    double value = b[0] + b[1] * cp(0) + b[2] * cp(1) + b[3] * cp(0) * cp(1);
+
+    _var.setNodalValue(value);
+  }
+  else
+  {
+    Real value = 0;
+    for (_qp = 0; _qp < _qrule->n_points(); _qp++)
+      value += _JxW[_qp] * _coord[_qp] * _stress[_qp](_i, _j);
+
+    value /= (_bnd ? _current_side_volume : _current_elem_volume);
+    _var.setNodalValue(value);
+  }
+}
